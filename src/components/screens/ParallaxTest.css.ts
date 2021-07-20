b3cc8fc85c37b88dce49d71fa381674703e7882c
TeXmeijin <i8858garden@gmail.com>
2021-07-20 09:48:51 +0900

    testing of react-parallax

--- src/components/screens/ParallaxTest.css.ts
+++ src/components/screens/ParallaxTest.css.ts
@@ -3,6 +3,15 @@ import { style } from '@vanilla-extract/css';
 export const styles = {
   parallax: style({
     width: '100%',
-    minHeight: 300,
+    height: 600,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }),
+  text: style({
+    padding: 32,
+    fontWeight: 'bold',
+    fontSize: 24,
+    backgroundColor: 'white',
   }),
 };
--- src/components/screens/ParallaxTest.tsx
+++ src/components/screens/ParallaxTest.tsx
@@ -1,14 +1,63 @@
-import { Background, Parallax } from 'react-parallax';
+import { Parallax } from 'react-parallax';
 import { styles } from '~/components/screens/ParallaxTest.css';
 
 export const ParallaxTest = () => {
   return (
     <div>
       <h1>Parallax Test</h1>
-      <Parallax strength={300}>
-        <Background className={styles.parallax}>
-          <img src="http://www.fillmurray.com/500/320" alt="fill murray" />
-        </Background>
+      <Parallax
+        bgImage={
+          'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/empire-state-building-black-and-white-square-format-john-farnan.jpg'
+        }
+        strength={300}
+      >
+        <div className={styles.parallax}>
+          <span className={styles.text}>パララックス</span>
+        </div>
+      </Parallax>
+      <Parallax
+        bgImage={
+          'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/empire-state-building-black-and-white-square-format-john-farnan.jpg'
+        }
+        strength={500}
+        renderLayer={(percentage) => (
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              fontSize: '2rem',
+              width: '500vw',
+              height: '600px',
+              // position: 'absolute',
+              background: `rgba(255, 125, 0)`,
+              transform: `translateX(${(percentage - 1) * 100 - 80}vw)`,
+              // left: '50%',
+              // top: '50%',
+              // borderRadius: '50%',
+              // transform: 'translate(-50%,-50%)',
+              // width: percentage * 500,
+              // height: percentage * 500,
+            }}
+          >
+            <span>
+              aqwscxczvdfertgjbngkhyoaqwscxczvdfertgjbngkhyoaqwscxczvdfertgjbngkhyoaqwscxczvdfertgjbngkhyoaqwscxczvdfertgjbngkhyoaqwscxczvdfertgjbngkhyoaqwscxczvdfertgjbngkhyoaqwscxczvdfertgjbngkhyoaqwscxczvdfertgjbngkhyo
+            </span>
+          </div>
+        )}
+      >
+        <div className={styles.parallax}>
+          <span className={styles.text}>パララックス</span>
+        </div>
+      </Parallax>
+      <Parallax
+        bgImage={
+          'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/empire-state-building-black-and-white-square-format-john-farnan.jpg'
+        }
+        strength={100}
+      >
+        <div className={styles.parallax}>
+          <span className={styles.text}>パララックス</span>
+        </div>
       </Parallax>
     </div>
   );
